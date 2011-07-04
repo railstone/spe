b99475f5af933643a4e7ea576298aff27bf2ed64
railstone <mark26acc@gmail.com>
2011-07-04 20:30:38 +0200

    Initial spe

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Spe::Application.config.secret_token = 'd77b10cf5e138e5ba9af82039a9926ec91803d3f5bd428b2a71a90536605e229cc15fd0bf449c3aa88829ed74478301ae7499e22a83ca6e1909392c4d757c372'
